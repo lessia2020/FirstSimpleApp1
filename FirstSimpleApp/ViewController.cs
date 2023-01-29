c60a6d6d3f4a6037dc47e33f6b108ad09f4c0edd
lessia2020 <nurislamova2000@gmail.com>
2023-01-29 10:10:18 +0100

    desktop

---
+++ FirstSimpleApp/ViewController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace FirstSimpleApp
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+
+        partial void PushButton(NSButton sender)
+        {
+			Console.WriteLine("Button pushed");
+        }
+        partial void ResetButton(NSButton sender)
+        {
+			Console.WriteLine("Reset");
+        }
+    }
+}
